fd70dd23187d13262f2969e7e92096e088243bd2
ZidaneCode <redjdalzidane01@gmail.com>
2025-03-07 18:52:32 +0100

    first commit

---
+++ src/test/java/com/rhapplication/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhapplication.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
